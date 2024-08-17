1813b4acb16f9d720cd5445ffae3b6179355801c
Screamer <vanagoncharov@mail.ru>
2024-08-17 13:45:47 +0300

    Initial commit with gradle dependencies and project structure

--- app/src/main/java/ideaplatform/test_task/ideaplatform/MainActivity.kt
+++ app/src/main/java/ideaplatform/test_task/ideaplatform/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ideaplatform.test_task.ideaplatform.ui.screen.MainScreen
 import ideaplatform.test_task.ideaplatform.ui.theme.IdeaPlatformTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             IdeaPlatformTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
---
+++ app/src/main/java/ideaplatform/test_task/ideaplatform/ui/ui_kit/CustomChip.kt
@@ -0,0 +1,8 @@
+package ideaplatform.test_task.ideaplatform.ui.ui_kit
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomChip() {
+
+}
